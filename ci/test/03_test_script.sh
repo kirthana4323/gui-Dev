d5bff24f5fa714f1b67fcb1998af8d20322a4098
kirthana4323 <kirthana.s@vlinder.io>
2024-08-27 10:07:40 +0530

    Update 03_test_script.sh

--- ci/test/03_test_script.sh
+++ ci/test/03_test_script.sh
@@ -63,12 +63,12 @@ if [ "$RUN_FUZZ_TESTS" = "true" ]; then
     echo "Using qa-assets repo from commit ..."
     git log -1
   )
-elif [ "$RUN_UNIT_TESTS" = "true" ] || [ "$RUN_UNIT_TESTS_SEQUENTIAL" = "true" ]; then
-  export DIR_UNIT_TEST_DATA=${DIR_QA_ASSETS}/unit_test_data/
-  if [ ! -d "$DIR_UNIT_TEST_DATA" ]; then
-    mkdir -p "$DIR_UNIT_TEST_DATA"
-    ${CI_RETRY_EXE} curl --location --fail https://github.com/bitcoin-core/qa-assets/raw/main/unit_test_data/script_assets_test.json -o "${DIR_UNIT_TEST_DATA}/script_assets_test.json"
-  fi
+# elif [ "$RUN_UNIT_TESTS" = "true" ] || [ "$RUN_UNIT_TESTS_SEQUENTIAL" = "true" ]; then
+#   export DIR_UNIT_TEST_DATA=${DIR_QA_ASSETS}/unit_test_data/
+#   if [ ! -d "$DIR_UNIT_TEST_DATA" ]; then
+#     mkdir -p "$DIR_UNIT_TEST_DATA"
+#     ${CI_RETRY_EXE} curl --location --fail https://github.com/bitcoin-core/qa-assets/raw/main/unit_test_data/script_assets_test.json -o "${DIR_UNIT_TEST_DATA}/script_assets_test.json"
+#   fi
 fi
 
 if [ "$USE_BUSY_BOX" = "true" ]; then
