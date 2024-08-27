5712de1958e05b3b933ba7c4de33bc38546d3ce2
kirthana4323 <kirthana.s@vlinder.io>
2024-08-27 16:36:24 +0530

    Update 03_test_script.sh

--- ci/test/03_test_script.sh
+++ ci/test/03_test_script.sh
@@ -123,7 +123,7 @@ cd "${BASE_BUILD_DIR}"
 
 bash -c "${BASE_ROOT_DIR}/configure --cache-file=config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG" || ( (cat config.log) && false)
 
-# make distdir VERSION="$HOST"
+make distdir VERSION="$HOST"
 
 # cd "${BASE_BUILD_DIR}/bitcoin-$HOST"
 
