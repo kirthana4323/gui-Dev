26a13aa722d4914c27b5e7e28c247ac6b38578c2
kirthana4323 <kirthana.s@vlinder.io>
2024-08-28 15:19:33 +0530

    Update 03_test_script.sh

--- ci/test/03_test_script.sh
+++ ci/test/03_test_script.sh
@@ -124,9 +124,9 @@ cd "${BASE_BUILD_DIR}"
 bash -c "${BASE_ROOT_DIR}/configure --cache-file=config.cache $BITCOIN_CONFIG_ALL $BITCOIN_CONFIG" || ( (cat config.log) && false)
 
 cd /ci_container_base/src/sphincsplus
+./configure
+make -j7
 ls -al .libs/libsphincsplus.la
-# ./configure
-# make -j7
 
 # ls -al
 
